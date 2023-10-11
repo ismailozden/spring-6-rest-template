ca631cd0f8a44ae2a880dcb02147d06c692377a8
Ismail Ozden <ismailozden.de@gmail.com>
2023-10-11 13:11:35 +0200

    Refactor listBeers method and add test for it.

--- src/test/java/com/zdn/zdn/client/BeerClientImplTest.java
+++ src/test/java/com/zdn/zdn/client/BeerClientImplTest.java
@@ -1,12 +1,17 @@
 package com.zdn.zdn.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
+        beerClient.listBeers();
     }
 }

abbdca0a73745cc2726d790bad0d01fee008f721
Ismail Ozden <ismailozden.de@gmail.com>
2023-10-11 16:20:21 +0200

    Add getBeerById method / path param.

--- src/main/java/com/zdn/zdn/client/BeerClient.java
+++ src/main/java/com/zdn/zdn/client/BeerClient.java
@@ -4,9 +4,12 @@ import com.zdn.zdn.model.BeerDTO;
 import com.zdn.zdn.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface BeerClient  {
 
     Page<BeerDTO> listBeers();
     Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize);
 
+    BeerDTO getBeerById(UUID beerId);
 }
--- src/test/java/com/zdn/zdn/client/BeerClientImplTest.java
+++ src/test/java/com/zdn/zdn/client/BeerClientImplTest.java
@@ -1,8 +1,11 @@
 package com.zdn.zdn.client;
 
+import com.zdn.zdn.model.BeerDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -23,6 +26,16 @@ class BeerClientImplTest {
     @Test
     void listBeersWithPageSize() {
         beerClient.listBeers(null, null, false, 1, 20);
+
+    }
+
+    @Test
+    void getBeerById() {
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        BeerDTO beerDTO = beerDTOS.getContent().get(0);
+        BeerDTO byIdBeerDTO = beerClient.getBeerById(beerDTO.getId());
+        Assertions.assertNotNull(byIdBeerDTO);
 
     }
 }
